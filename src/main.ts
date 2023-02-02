ceb529adf96222987a5ff3a477c1aa17cd657a97
prismathic <pjjsalam1999@gmail.com>
2023-02-02 14:26:29 +0100

    create auth and user module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
