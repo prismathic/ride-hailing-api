339c36eb2907e8e2cf01d355aa932204dee53cf8
prismathic <pjjsalam1999@gmail.com>
2023-02-06 13:46:48 +0100

    complete test for user service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
+const singleUser = { email: 'email@email.com' };
+
 describe('UsersService', () => {
-  let service: UsersService;
+  let userService: UsersService;
+  let userRepository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            findOneBy: jest.fn().mockResolvedValue(singleUser),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    userService = module.get<UsersService>(UsersService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should get a single user by email', async () => {
+      const emailForTest = 'email@email.com';
+      const userRepositorySpy = jest.spyOn(userRepository, 'findOneBy');
+
+      expect(await userService.findOne(emailForTest)).toEqual(singleUser);
+      expect(userRepositorySpy).toBeCalledWith({ email: emailForTest });
+    });
   });
 });
